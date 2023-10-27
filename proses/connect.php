e7c6c4a0d40d5d325dd28b742361c9238521afb2
Fadli Maulana <127267181+fadllyyy@users.noreply.github.com>
2023-10-27 22:15:32 +0700

    Update connect.php

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","db_decafe");
+    $conn = mysqli_connect("localhost","u837952942_fadlymaulana","5Rj+MRE[d","db_decafe");
     if(!$conn){
         echo "Gagal koneksi";
     }
